cc06f1657e9aa3c4ba90b4d60c5b8cd8a7d20712
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-02-01 19:21:21 +0000

    Lab 0: Redmine and Development Environment
    Redmine e Ambiente de Desenvolvimento

---
+++ lab0/hello.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : hello.c
+ Author      : Adriana
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Hello World! :D");
+	return EXIT_SUCCESS;
+}
